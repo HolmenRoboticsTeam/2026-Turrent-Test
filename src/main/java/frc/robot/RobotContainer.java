0e0da6abdea333a91b83cdbbd8999de0e4bee66f
ranwar26 <warrickdogo@gmail.com>
2026-01-20 13:52:17 -0600

    Added TurretSubsystem and controller to RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,30 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
+
+  private TurretSubsystem turret = new TurretSubsystem();
+
+  private CommandXboxController controller = new CommandXboxController(0);
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+    turret.setDefaultCommand(Commands.run(() -> {
+      turret.setTargetSpeed(12.0 * controller.getLeftTriggerAxis());
+
+      turret.setTargetRotation(Rotation2d.fromRotations(0.5 * controller.getLeftX()));
+
+    }, turret));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
